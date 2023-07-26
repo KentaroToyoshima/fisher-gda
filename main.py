fdf5f6c032ef3c564fd07bfe28d3385744fee93a
KentaroToyoshima <toyokenn.11909190@gmail.com>
2023-07-26 12:11:47 +0900

    sqrt(t)倍を追加

--- main.py
+++ main.py
@@ -161,6 +161,12 @@ def plot_and_save_obj_graphs(obj_hist_data, plot_titles, market_types, dir_obj, 
 
         for i, (obj_hist, title, market_type) in enumerate(zip(obj_hist_data, plot_titles, market_types)):
             mean_obj = np.mean(obj_hist, axis=0) - np.min(np.mean(obj_hist, axis=0))
+            #Goktasによると、下の式はt^{-1/2}で割るということらしい
+            mean_obj = mean_obj.flatten()
+            indices = np.arange(1, len(mean_obj)+1, 1)
+            indices = indices ** (1/2)
+            mean_obj = mean_obj * indices
+            ### ここまで
             axs[i].plot(mean_obj, color="b")
             axs[i].set_title(title, fontsize="medium")
             axs[i].set_xlabel('Iteration Number', fontsize=21)
